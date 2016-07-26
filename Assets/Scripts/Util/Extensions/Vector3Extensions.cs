f330b4cfaee27e1af65947c663cbd14162564e3d
Ândrei d`Oliveira Mesquita Schuch <andreirs@outlook.com>
2016-07-26 16:03:14 -0300

    Adicionado pacote Util
    Adicionado pacote Util, e seus subpacotes Editor e Extensions;
    
    Em Editor, foi adicionado o ReadOnlyAttribute que serve para apresentar
    um atributo de uma classe, no inspector do Unity, de forma que não seja
    possível sua edição pelo inspector;
    
    Em Extensions, foi adicionado a classe Vector3Extensions que serve como
    extensão às funcionalidades classe Vector3.

---
+++ Assets/Scripts/Util/Extensions/Vector3Extensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Megaman.Util.Extensions
+{
+    /// <summary>
+    /// Métodos de extensão para a classe Vector3.
+    /// </summary>
+    public static class Vector3Extensions
+    {
+        /// <summary>
+        /// Transforma um Vector3 em um Vector2, retirando o eixo Z.
+        /// </summary>
+        /// <param name="vector3"></param>
+        /// <returns></returns>
+        public static Vector2 ToVector2(this Vector3 vector3)
+        {
+            return new Vector2(vector3.x, vector3.y);
+        }
+
+    }
+}
